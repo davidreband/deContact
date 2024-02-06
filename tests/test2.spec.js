af9a6b4118defb462910e686bf7f7cb0bacb7cc2
David Reband <david.reband@webanizer.de>
2024-02-06 10:24:06 +0100

    test 12

--- tests/test2.spec.js
+++ tests/test2.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { chromium } from 'playwright';
 
 
-test('test', async () => {
+test('test', async ({ page }) => {
 		test.setTimeout(60000);
    await page.goto('http://localhost:5173/');
 	
